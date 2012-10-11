019448b2d0543027df6721c9ed62713e1df9f755
Mitch Friedman <mitchfriedman5@gmail.com>
2012-10-10 22:00:59 -0400

    Completed Activate.php link + css

--- activate.php
+++ activate.php
@@ -5,13 +5,27 @@ if(isset($_GET['activate']))
 {
 	GetUserFromActivate($_GET['activate']);
 }
+else
+{
+	header("Location: ../index.php");
+}
 
 ?>
 
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="css/activate.css"/>
+<meta http-equiv="REFRESH" content="5;url=index.php"></HEAD>
+
 </head>
 <body>
-<p>thanks for activating</p>
+<div id="cnt_top">
+<h1>Study Buddy</h1>
+</div>
+<div id="main">
+
+<h1>Thanks for activating your account. You will be redirected shortly, or click <a href="index.php">here</a> to log in. </h1>
+
+</div>
 </body>
 </html>
