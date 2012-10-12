61ebdb42dc586d2a95531251e67f9f22a7e330bb
Mitch Friedman <mitchfriedman5@gmail.com>
2012-10-11 22:22:13 -0400

    Revert "Lots of save"
    This reverts commit af538e242c3e8dd952cbf4ef012288a2b7e03865.

--- core/login.php
+++ core/login.php
@@ -11,6 +11,7 @@ if(strlen($_POST['username'])>0 && strlen($_POST['password'])>0)
 			$password = $_POST['password'];
 			$password = base64_encode($password);
 	 		setcookie("username", $username, false,"/");
+			setcookie("password", md5($password), false,"/");
 			header('Location: ../profile.php');
 		}
 		else
--- profile.php
+++ profile.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_COOKIE['username']))
+if(isset($_COOKIE['username']) && isset($_COOKIE['password']))
 {
 	echo "logged in",base64_decode($_COOKIE['username']);
 	echo "<a href='core/logout.php'>Logout</a>";
